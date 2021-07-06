3230e05a878810629fbe46eab154a5791049ee29
BenjaminThio <74221883+DarkKnight73379@users.noreply.github.com>
2021-07-06 16:25:49 +0800

    Added 3D and working Lab Scene

---
+++ Path To Chemistry/Assets/Lab/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class Data
+{
+    public static string raycastObject { get; set; }
+}
+
+public class Player : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    public GameObject explosionEffect;
+    float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Data.raycastObject = "";
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            var potion = GameObject.Find("Flask").GetComponent<Transform>();
+            Data.raycastObject = selection.name;
+            GameObject.Find("Label1").GetComponent<Text>().text = Data.raycastObject;
+            if (Data.raycastObject == "Reaction")
+            {
+                GameObject.Find("Label2").GetComponent<Text>().text = "Flask Detected!";
+                Instantiate(explosionEffect, potion.position, potion.rotation);
+                Destroy(GameObject.Find("Flask"));
+            }
+            else
+            {
+                GameObject.Find("Label2").GetComponent<Text>().text = "";
+            }
+        }
+    }
+}
