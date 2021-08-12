e61936a7862a4dd460b290ac3f98634a6ab2f57b
Edmund <12859874+lightcmd@users.noreply.github.com>
2021-08-12 14:41:14 +0800

    Main: Terrain gen complete.

--- Path To Chemistry/Assets/Main/Scripts/Noise.cs
+++ Path To Chemistry/Assets/Main/Scripts/Noise.cs
@@ -1,75 +1,100 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Noise
-{
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistence, float lacunarity, Vector2 offset)
-    {
-        var noiseMap = new float[mapWidth, mapHeight];
+public static class Noise {
 
-        var prng = new System.Random(seed);
-        var octaveOffsets = new Vector2[octaves];
+	public enum NormalizeMode {Local, Global};
 
-        for (int i = 0; i < octaves; i++)
-        {
-            float offsetX = prng.Next(-100000, 100000) + offset.x;
-            float offsetY = prng.Next(-100000, 100000) + offset.y;
-            octaveOffsets[i] = new Vector2(offsetX, offsetY);
-        }
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, NoiseSettings settings, Vector2 sampleCentre) {
+		float[,] noiseMap = new float[mapWidth,mapHeight];
 
-        if (scale <= 0)
-        {
-            scale = 0.0001f;
-        }
+		System.Random prng = new System.Random (settings.seed);
+		Vector2[] octaveOffsets = new Vector2[settings.octaves];
 
-        float maxNoiseHeight = float.MinValue;
-        float minNoiseHeight = float.MaxValue;
+		float maxPossibleHeight = 0;
+		float amplitude = 1;
+		float frequency = 1;
 
-        float halfWidth = mapWidth / 2f;
-        float halfHeight = mapHeight / 2f;
-        
-        for (int y = 0; y < mapHeight; y++)
-        {
-            for (int x = 0; x < mapWidth; x++)
-            {
-                float amplitude = 1;
-                float frequency = 1;
-                float noiseHeight = 0;
-                
-                for (int i = 0; i < octaves; i++)
-                {
-                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
-                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+		for (int i = 0; i < settings.octaves; i++) {
+			float offsetX = prng.Next (-100000, 100000) + settings.offset.x + sampleCentre.x;
+			float offsetY = prng.Next (-100000, 100000) - settings.offset.y - sampleCentre.y;
+			octaveOffsets [i] = new Vector2 (offsetX, offsetY);
 
-                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
-                    noiseHeight += perlinValue * amplitude;
+			maxPossibleHeight += amplitude;
+			amplitude *= settings.persistance;
+		}
 
-                    amplitude *= persistence;
-                    frequency *= lacunarity;
-                }
+		float maxLocalNoiseHeight = float.MinValue;
+		float minLocalNoiseHeight = float.MaxValue;
 
-                if (noiseHeight > maxNoiseHeight)
-                {
-                    maxNoiseHeight = noiseHeight;
-                } else if (noiseHeight < minNoiseHeight)
-                {
-                    minNoiseHeight = noiseHeight;
-                }
-                
+		float halfWidth = mapWidth / 2f;
+		float halfHeight = mapHeight / 2f;
 
-                noiseMap[x, y] = noiseHeight;
-            }
-        }
 
-        for (int y = 0; y < mapHeight; y++)
-        {
-            for (int x = 0; x < mapWidth; x++)
-            {
-                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
-            }
-        }
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
 
-        return noiseMap;
-    }
+				amplitude = 1;
+				frequency = 1;
+				float noiseHeight = 0;
+
+				for (int i = 0; i < settings.octaves; i++) {
+					float sampleX = (x-halfWidth + octaveOffsets[i].x) / settings.scale * frequency;
+					float sampleY = (y-halfHeight + octaveOffsets[i].y) / settings.scale * frequency;
+
+					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
+					noiseHeight += perlinValue * amplitude;
+
+					amplitude *= settings.persistance;
+					frequency *= settings.lacunarity;
+				}
+
+				if (noiseHeight > maxLocalNoiseHeight) {
+					maxLocalNoiseHeight = noiseHeight;
+				} 
+				if (noiseHeight < minLocalNoiseHeight) {
+					minLocalNoiseHeight = noiseHeight;
+				}
+				noiseMap [x, y] = noiseHeight;
+
+				if (settings.normalizeMode == NormalizeMode.Global) {
+					float normalizedHeight = (noiseMap [x, y] + 1) / (maxPossibleHeight / 0.9f);
+					noiseMap [x, y] = Mathf.Clamp (normalizedHeight, 0, int.MaxValue);
+				}
+			}
+		}
+
+		if (settings.normalizeMode == NormalizeMode.Local) {
+			for (int y = 0; y < mapHeight; y++) {
+				for (int x = 0; x < mapWidth; x++) {
+					noiseMap [x, y] = Mathf.InverseLerp (minLocalNoiseHeight, maxLocalNoiseHeight, noiseMap [x, y]);
+				}
+			}
+	}
+
+		return noiseMap;
+	}
+
+}
+
+[System.Serializable]
+public class NoiseSettings {
+	public Noise.NormalizeMode normalizeMode;
+
+	public float scale = 50;
+
+	public int octaves = 6;
+	[Range(0,1)]
+	public float persistance =.6f;
+	public float lacunarity = 2;
+
+	public int seed;
+	public Vector2 offset;
+
+	public void ValidateValues() {
+		scale = Mathf.Max (scale, 0.01f);
+		octaves = Mathf.Max (octaves, 1);
+		lacunarity = Mathf.Max (lacunarity, 1);
+		persistance = Mathf.Clamp01 (persistance);
+	}
 }
--- Path To Chemistry/Assets/Main/Scripts/TextureGenerator.cs
+++ Path To Chemistry/Assets/Main/Scripts/TextureGenerator.cs
@@ -1,35 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class TextureGenerator
-{
-    public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height)
-    {
-        var texture = new Texture2D(width, height);
-        texture.filterMode = FilterMode.Point;
-        texture.wrapMode = TextureWrapMode.Clamp;
-        texture.SetPixels(colorMap);
-        texture.Apply();
-        return texture;
-    }
+public static class TextureGenerator {
 
-    public static Texture2D TextureFromHeightMap(float[,] heightMap)
-    {
-        int width = heightMap.GetLength(0);
-        int height = heightMap.GetLength(1);
+	public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) {
+		Texture2D texture = new Texture2D (width, height);
+		texture.filterMode = FilterMode.Point;
+		texture.wrapMode = TextureWrapMode.Clamp;
+		texture.SetPixels (colourMap);
+		texture.Apply ();
+		return texture;
+	}
 
-        Texture2D texture = new Texture2D(width, height);
 
-        var colorMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, heightMap[x, y]);
-            }
-        }
+	public static Texture2D TextureFromHeightMap(HeightMap heightMap) {
+		int width = heightMap.values.GetLength (0);
+		int height = heightMap.values.GetLength (1);
 
-        return TextureFromColorMap(colorMap, width, height);
-    }
+		Color[] colourMap = new Color[width * height];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				colourMap [y * width + x] = Color.Lerp (Color.black, Color.white, Mathf.InverseLerp(heightMap.minValue,heightMap.maxValue,heightMap.values [x, y]));
+			}
+		}
+
+		return TextureFromColourMap (colourMap, width, height);
+	}
+
 }
