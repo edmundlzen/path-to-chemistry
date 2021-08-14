06c1441c2eb3fbdbb3ee1da299d7d2ca97cb465c
BenjaminThio <74221883+DarkKnight73379@users.noreply.github.com>
2021-08-14 15:37:44 +0800

    Added Inventory(In Progress)

---
+++ Path To Chemistry/Assets/Chemidex/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    void Start()
+    {
+        elementCheck();
+    }
+    public void elementSymbol()
+    {
+        print("Hello World");
+    }
+    public void elementCheck()
+    {
+        var filePath = Path.Combine(Application.dataPath, "Elements.json");
+        var fileContent = File.ReadAllText(filePath);
+        var elementData = JsonConvert.DeserializeObject<ElementData>(fileContent);
+        for (int i = 1; i <= 118; i++)
+        {
+            GameObject.Find($"Slot ({i})").transform.GetComponentInChildren<Text>().text = elementData.elements.Keys.ElementAt(i - 1);
+        }
+    }
+}
