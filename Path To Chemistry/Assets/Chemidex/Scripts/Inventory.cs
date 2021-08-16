b50c879763f6644ef7d9b52eda8c9509e10635de
BenjaminThio <74221883+DarkKnight73379@users.noreply.github.com>
2021-08-16 21:52:28 +0800

    Inventory has finally done

--- Path To Chemistry/Assets/Chemidex/Scripts/Inventory.cs
+++ Path To Chemistry/Assets/Chemidex/Scripts/Inventory.cs
@@ -1,26 +1,83 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public static class InventoryData
+{
+    public static string Slot { get; set; }
+    public static string Input { get; set; }
+}
 
 public class Inventory : MonoBehaviour
 {
     void Start()
     {
         Load();
+        Load2();
         elementCheck();
+        InventoryData.Slot = "Slot (1)";
+        GameObject.Find(InventoryData.Slot).GetComponent<Image>().color = Color.cyan;
         var elementData = ElementData.Instance();
         var playerData = PlayerData.Instance();
+        for (int i = 1; i <= 118; i++)
+        {
+            if (InventoryData.Slot == $"Slot ({i})")
+            {
+                var Name = playerData.Inventory.Keys.ElementAt(i - 1);
+                var Quantity = playerData.Inventory.Values.ElementAt(i - 1);
+                GameObject.Find("State").GetComponent<Text>().text = $"Name: {Name}\nQuantity: {Quantity}";
+            }
+        }
+/*
         for (int i = 1; i <= 118; i++)
         {
             playerData.Inventory.Add(elementData.elements.Keys.ElementAt(i - 1), 0);
         }
         Save();
+*/
+    }
+    public void quantityManager(string Input)
+    {
+        InventoryData.Input = Input;
+    }
+    public void Done()
+    {
+        for (int i = 1; i <= 118; i++)
+        {
+            var playerData = PlayerData.Instance();
+            if (Convert.ToInt32(InventoryData.Input) > 100)
+            {
+                GameObject.Find("Info").GetComponent<Text>().text = "Max";
+            }
+            else if (InventoryData.Slot == $"Slot ({i})")
+            {
+                if ((Convert.ToInt32(InventoryData.Input) <= playerData.Inventory[playerData.Inventory.Keys.ElementAt(i - 1)]))
+                {
+                    GameObject.Find("Info").GetComponent<Text>().text = "Min";
+                }
+            }
+        }
     }
     public void elementSymbol()
     {
-        print("Hello World");
+        var playerData = PlayerData.Instance();
+        GameObject.Find(InventoryData.Slot).GetComponent<Image>().color = Color.white;
+        InventoryData.Slot = EventSystem.current.currentSelectedGameObject.name;
+        GameObject.Find(InventoryData.Slot).GetComponent<Image>().color = Color.cyan;
+        for (int i = 1; i <= 118; i++)
+        {
+            if (EventSystem.current.currentSelectedGameObject.name == $"Slot ({i})")
+            {
+                var Name = playerData.Inventory.Keys.ElementAt(i - 1);
+                var Quantity = playerData.Inventory.Values.ElementAt(i - 1);
+                GameObject.Find("State").GetComponent<Text>().text = $"Name: {Name}\nQuantity: {Quantity}";
+                break;
+            }
+        }
     }
     public void elementCheck()
     {
@@ -52,5 +109,13 @@ public class Inventory : MonoBehaviour
         var fileContent = File.ReadAllText(filePath);
         var elementData = JsonConvert.DeserializeObject<ElementData>(fileContent);
         ElementData.Instance().UpdateElementData(elementData);
+    }
+    void Load2()
+    {
+        string directory = $"{Application.persistentDataPath}/Data";
+        var filePath = Path.Combine(directory, "Saves.json");
+        var fileContent = File.ReadAllText(filePath);
+        var playerData = JsonConvert.DeserializeObject<PlayerData>(fileContent);
+        PlayerData.Instance().UpdatePlayerData(playerData);
     }
 }
